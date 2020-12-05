65a64996af89e8276c8fd1e56783f2fe0b2f8574
NissyP <ishika.12.twelve@gmail.com>
2020-12-05 13:42:43 +0000

    [Finish] ex-4

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   
   root 'homes#top'
   get 'home/about' => 'homes#about'
+
+  post 'follow/:id' => 'relationships#follow', as: 'follow' 
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' 
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    get 'follower' => 'relationships#follower'
+    get 'followed' => 'relationships#followed'
+  end
+  
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
